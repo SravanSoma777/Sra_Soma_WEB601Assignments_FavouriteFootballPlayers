b30284d4221ded94a396bbe4ba7e93f57b4df7f1
SravanSoma777 <W0766138@myscc.ca>
2023-02-16 06:29:33 -0500

    styles refactor

--- sra_soma_myfavfootballplayers/src/app/hover-affect.directive.ts
+++ sra_soma_myfavfootballplayers/src/app/hover-affect.directive.ts
@@ -11,33 +11,20 @@ export class HoverAffectDirective {
    }
   @Input() highlightType?: "type" | "tag";
  
-  /* @HostListener('mouseenter') onMouseEnter() {
-    this.el.nativeElement.style.textDecoration = 'underline';
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.el.nativeElement.style.textDecoration = 'none';
-  }
-
-  @HostListener('mouseover') onMouseOver(){
-    if(this.hightlightTag === 'tag'){
-      this.el.nativeElement.style.color = 'red';
-    } 
-  }*/
   @HostListener("mouseover") onMouseOver() {
     if (this.highlightType === "type") {
-      // set type background color
+     
       this.el.nativeElement.style.textDecoration = "underline";
     }
 
     if (this.highlightType === "tag") {
-      // set tag font color
+      
       this.el.nativeElement.style.fontWeight = '900';
     }
   }
   @HostListener("mouseout") onMouseOut() {
     if (this.highlightType === "type") {
-      // reset type background color
+      
       this.el.nativeElement.style.textDecoration = "none"
     }
 
